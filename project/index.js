943584056f1394082e8d422b057ab286f992094a
Isiaka Abdulahi <isiakaabd@gmail.com>
2021-03-05 23:07:21 +0100

    adding more and more features

--- project/index.js
+++ project/index.js
@@ -39,21 +39,22 @@ const scale = (num, in_min, in_max, out_min, out_max) => {
 //
 
 
-// const header =document.getElementById("header");
-// const title = document.getElementById("title");
+ const imageContainer =document.getElementById("image-container");
+ const AboutMe = document.getElementById("aboutMe");
 // const content = document.getElementById("content");
-// const profile_image = document.getElementById("profile-image");
-// const ProfileName = document.getElementById("name");
+ const pin = document.getElementById("pin");
+ const mySelf = document.getElementById("myself");
 // const date = document.getElementById("date");
 
 
 
 function getInfo(){
-    // header.innerHTML=`<img src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80" alt="image header by isiaka Abdulahi">`;
-    // title.innerText= "Welcome To Remlad Ventures";
+    imageContainer.innerHTML=`<img src="/project/ISIAKA.png" class="w-100" alt="Isiaka Abdulahi">`;
+     AboutMe.innerText= "About Me";
     // content.innerText= "REMLAD DATA is a reliable web to buy Cheap SME Data & Direct Data Bundles, Airtime VTU, Cable Subscriptions, Pay Electricity Bills & More..."
-    // profile_image.innerHTML= `<img src="/ISIAKA.png" alt="REMLAD CEO">`  
-    // ProfileName.innerText ="ISIAKA ABDULAHI"
+   pin.innerHTML= ` <img src="https://previews.123rf.com/images/headoff/headoff1306/headoff130600011/20364438-a-colourful-3d-rendered-notice-board-pin-illustration.jpg" alt="pin">`  
+   mySelf.innerText ="Hi! I'm  Isiaka Abdulahi. A Frontend Developer with a focus on building fast, robust, SEO optimized and optimized User Experiences. I believe in developing Interfaces which bring a smile."
+     
     // date.innerHTML = 'Feb 21, 2021'
 
 animated_bg.forEach((bg) => bg.classList.remove("animated-bg"))
